f0190fd8b9ba2e2db88d01eb1d7a54148f8e9dd6
Furz1q <serbek200@mail.ru>
2024-12-11 05:46:19 +0300

    end

---
+++ src/main/java/service/DepartamentServise.java
@@ -0,0 +1,45 @@
+package service;
+
+import domain.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class DepartamentServise {
+    private EmployeeService employeeService;
+
+    public DepartamentServise(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    public Employee getEmployeeWithMaxSalaryByDepartment(String department) {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .max(Comparator.comparingInt(Employee::getSalary))
+                .orElse(null);
+    }
+    public Employee getEmployeeWithMinSalaryByDepartment(String department) {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .min(Comparator.comparingInt(Employee::getSalary))
+                .orElse(null);
+    }
+    public Map<String, List<Employee>> getAllEmployeesByDepartment(String department) {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return employees.stream()
+                .filter(employee -> employee.getDepartment().equals(department))
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
+    public Map<String, List<Employee>> getAllEmployeesByDepartments() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
+
+}
